ad9124c270a3e597bd2e4fa2b1fbdba10a6db28d
Bogdan Toader <bogdaaan.t@gmail.com>
2016-03-15 17:12:52 +0000

    hi

--- dfdt.m
+++ dfdt.m
@@ -1,7 +1,10 @@
-function dxv = dfdt(xv, g)
+function dxv = dfdt(xv, Fmfunc, g)
+  
+  fm = Fmfunc(xv);
+
   dxv = zeros(4,1);
   dxv(1) = xv(3);
   dxv(2) = xv(4);
-  dxv(3) = 0;
-  dxv(4) = - g; 
+  dxv(3) = 0 + fm(1);
+  dxv(4) = - g + fm(2); 
 end
--- solveTheOde.m
+++ solveTheOde.m
@@ -2,15 +2,23 @@ function [] = solveTheOde()
 
 clear all; 
 
+g = 9.8;
+m = 0.5;
+rho = 1.2;
+r = 0.11;
+rev = 8;
+
 tspan = [0 5];
 xv0 = [0;0; 5; 5];
-g = 9.8;
+
+
+fmfunc = @(xv) F_magnussen(xv(1:2,:), xv(3:4,:), m, rho, r, rev);
 
 
 opts=odeset('Events',@stoppingevent,'OutputFcn',@odeplot);
 
 
-[t,xv] = ode45(@(t,xv) dfdt(xv,g), tspan, xv0, opts);
+[t,xv] = ode45(@(t,xv) dfdt(xv,fmfunc, g), tspan, xv0, opts);
 close all;
 
 plot(xv(:,1), xv(:,2))
