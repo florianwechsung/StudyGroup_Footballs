541e8e84b599741a4a4e8d92494437632e67a147
Bogdan Toader <bogdaaan.t@gmail.com>
2016-03-15 17:18:49 +0000

    mmmm

--- dfdt.m
+++ dfdt.m
@@ -1,10 +1,11 @@
-function dxv = dfdt(xv, Fmfunc, g)
+function dxv = dfdt(xv, Fmfunc, DragFunc, g)
   
   fm = Fmfunc(xv);
+  drag = DragFunc(xv);
 
   dxv = zeros(4,1);
   dxv(1) = xv(3);
   dxv(2) = xv(4);
-  dxv(3) = 0 + fm(1);
-  dxv(4) = - g + fm(2); 
+  dxv(3) = 0 + fm(1) + drag(1);
+  dxv(4) = - g + fm(2) + drag(2); 
 end
--- solveTheOde.m
+++ solveTheOde.m
@@ -7,18 +7,23 @@ m = 0.5;
 rho = 1.2;
 r = 0.11;
 rev = 8;
+coef = 0.47;
+den = 1.2;
+
 
 tspan = [0 5];
 xv0 = [0;0; 5; 5];
 
 
 fmfunc = @(xv) F_magnussen(xv(1:2,:), xv(3:4,:), m, rho, r, rev);
+dragfunc = @(xv) drag(coef,xv(3:4),den,m,r);
+
 
 
 opts=odeset('Events',@stoppingevent,'OutputFcn',@odeplot);
 
 
-[t,xv] = ode45(@(t,xv) dfdt(xv,fmfunc, g), tspan, xv0, opts);
+[t,xv] = ode45(@(t,xv) dfdt(xv,fmfunc, dragfunc, g), tspan, xv0, opts);
 close all;
 
 plot(xv(:,1), xv(:,2))
