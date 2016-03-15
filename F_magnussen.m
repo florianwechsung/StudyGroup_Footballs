9554d1235dbe6178495191a4a342017f9622bf6b
florianwechsung <f.wechsung@gmail.com>
2016-03-15 17:12:22 +0000

    fix

--- F_magnussen.m
+++ F_magnussen.m
@@ -1,7 +1,7 @@
 function [ acc ] = F_magnussen( x, x_dot, m, rho, r, rev)
     G = rev*(2*pi*r)^2;
     % Note: We set L=r
-    v = abs(x_dot);
+    v = norm(x_dot);
     F_magnitude = rho*v*G*r;
     acc = (F_magnitude/m) * [-x_dot(2);x_dot(1)] / norm(x_dot);
 end
