2deed285ee7607827b89cdc10d681f23a75a73b9
Bogdan Toader <bogdaaan.t@gmail.com>
2016-03-17 19:01:05 +0000

    moar

--- plotstuff.m
+++ plotstuff.m
@@ -1,12 +1,14 @@
 solveTheOde;
 analytical;
-plot(-xv(:,2), xv(:,1),'b', 'LineWidth', 2)
+pl = plot(-xv(:,2), xv(:,1),'b', 'LineWidth', 2);
 hold on;
+plot(x0,y, 'm', 'LineWidth', 2)
 plot(x, y, 'r', 'LineWidth', 2)
-lgd = legend('numerical', 'analytical');
+lgd = legend('numerical', 'analytical 1st order', 'analytical 2nd order');
 set(lgd, 'FontSize', 20);
 xlabel('lateral deflection', 'FontSize', 20);
 ylabel('kicking direction', 'FontSize', 20);
+
 %print('numericalVsAnalytical', '-depsc');
 
 
